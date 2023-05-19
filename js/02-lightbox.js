7c22385d85a7ca63c0ccfcb46534c539a858266c
Taras Bondar <tarasbondar2@gmail.com>
2023-05-19 23:26:18 +0300

    dz

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const cardsMarkup = createGalleryItemCardMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+const lightbox = new SimpleLightbox(".gallery li a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGalleryItemCardMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+            </a>
+        </li>
+        `;
+    })
+    .join("");
+}
